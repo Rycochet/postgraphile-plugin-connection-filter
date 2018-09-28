6acf75c3f4b7fc87873ffc2a8368e8cfaece2425
Katarina Golbang <golbang.k@gmail.com>
2018-09-28 18:36:14 +0200

    setup postgis operator; add more operators: disjointsG, intersectsG

--- src/PgConnectionArgFilterPostgisOperatorsPlugin.js
+++ src/PgConnectionArgFilterPostgisOperatorsPlugin.js
@@ -13,6 +13,30 @@ module.exports = function PgConnectionArgFilterPostgisOperatorsPlugin(builder) {
         allowedListTypes: ["NonList", "List"],
       }
     );
+    addConnectionFilterOperator(
+      "disjointsG",
+      "Returns TRUE if the Geometries do not 'spatially intersect' - if they do not share any space together.",
+      fieldType => fieldType,
+      (identifier, val) => {
+        return sql.query`ST_Disjoint(${identifier}, ${val}::geometry)`;
+      },
+      {
+        allowedFieldTypes: ["Geometry", "Geography", "String"],
+        allowedListTypes: ["NonList", "List"],
+      }
+    );
+    addConnectionFilterOperator(
+      "intersectsG",
+      "Returns TRUE if the Geometries/Geography 'spatially intersect in 2D' - (share any portion of space).",
+      fieldType => fieldType,
+      (identifier, val) => {
+        return sql.query`ST_Intersects(${identifier}, ${val}::geometry)`;
+      },
+      {
+        allowedFieldTypes: ["Geometry", "Geography", "String"],
+        allowedListTypes: ["NonList", "List"],
+      }
+    );
     return _;
   });
 };
